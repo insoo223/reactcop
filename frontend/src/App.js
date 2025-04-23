9b9036194d3ce4d56886a0f6917bb7088f91699e
insoo223 <insoo@hotmail.com>
2025-04-23 18:51:44 +0900

    init success

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect ( () => {
+        const fetchUsers = async () => {
+            try {
+                const res = await axios.get ('http://192.168.0.64:5000/api/users');
+                setUsers(res.data);
+            } catch (err) {
+                console.log ('Error fetching users: ', err);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+  return (
+    <div> 
+        <h1>User List</h1>
+        <ul>
+            {users.map (user => (
+                <li key={user._id}>
+                    {user.name}, {user.email}
+                </li>
+            ))}
+        </ul>
+    </div>
+  );
+}
+
+export default App;
