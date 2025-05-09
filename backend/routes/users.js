f66896dad5a2bbece42de89276b85406e303c627
insoo223 <insoo@hotmail.com>
2025-05-10 07:40:24 +0900

    Success pub internet 2 local RPi, PN 414457081

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,14 +12,5 @@ router.get ('/', async (req, res) => {
     }
 });
 
-router.get ('/api/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json ({error: err.message});
-    }
-});
-
 module.exports = router;
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,20 @@ import axios from 'axios';
 
 function App() {
     const [users, setUsers] = useState([]);
-    const MGURI = 'https://8bd2-118-35-64-189.ngrok-free.app';
+    const MGURI = 'https://d991-118-35-64-189.ngrok-free.app';
     useEffect ( () => {
         const fetchUsers = async () => {
             try {
                 console.log ('Insoo Backend Mongo URI: ');
-                console.log (`${MGURI}`);
-                //const res = await axios.get (`${MGURI}/api/users`)
+                //console.log (`${MGURI}`);
+                const res = await axios.get(`${MGURI}/api/users`, { headers: { "ngrok-skip-browser-warning": "true" } });
+
                 //const res = await axios.get (`${MGURI}`)
                 //        .then(response => console.log("Insoo axio Success: " + response.data))
                 //        .catch(error => console.error(error));
 
-                const res = await axios.get ('http://localhost:5000/api/users');
+                //const res = await axios.get ('http://localhost:5000/api/users');
+                //const res = await axios.get ('https://b196-118-35-64-189.ngrok-free.app/api/users');
                 //const res = await axios.get ('http://172.30.1.95:5000/api/users');
                 //const res = await axios.get ('http://192.168.0.64:5000/api/users');
                 setUsers(res.data);
